faf647aac6e0f0a6d039fe9035bbf04d59bb7da4
Catia Gomes <cgo@codehousegroup.com>
2022-09-03 23:46:43 +0100

    add tip to totalbill, update quotes for consistency (#9)

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,14 +1,24 @@
-import React, { useState } from 'react';
-import Header from '../Header';
-import Body from '../Body';
-import Footer from '../Footer';
-import { DEFAULT_TIP, DEFAULT_SPLIT } from '../../settings';
+import React, { useState } from "react";
+import Header from "../Header";
+import Body from "../Body";
+import Footer from "../Footer";
+import { DEFAULT_TIP, DEFAULT_SPLIT, DEFAULT_BILL } from "../../settings";
+import { getTotalTip, getTotalSplit, getSplitTip, getTotalBill } from "../../utils/calculate";
 
 const App = () => {
-  const [totalBill, setTotalBill] = useState(100);
-  const [totalSplit, setTotalSplit] = useState(totalBill / DEFAULT_SPLIT);
-  const [totalTip, setTotalTip] = useState(totalBill * (DEFAULT_TIP / 100));
-  const [splitTip, setSplitTip] = useState(totalBill / DEFAULT_SPLIT * (DEFAULT_TIP / 100));
+  /* initial calculations */
+  const storeTotalBill = getTotalBill(DEFAULT_BILL, DEFAULT_TIP);
+  const storeTotalTip = getTotalTip(DEFAULT_BILL, DEFAULT_TIP);
+  const storeTotalSplit = getTotalSplit(storeTotalBill, DEFAULT_SPLIT);
+  const storeSplitTip = getSplitTip(storeTotalTip, DEFAULT_SPLIT);
+
+ /* initial state */
+  const [totalBill, setTotalBill] = useState(storeTotalBill);
+  const [totalSplit, setTotalSplit] = useState(storeTotalSplit);
+  const [totalTip, setTotalTip] = useState(storeTotalTip);
+  const [splitTip, setSplitTip] = useState(storeSplitTip);
+
+  /* getters and setters */
   const get = { totalBill, totalSplit, totalTip, splitTip };
   const set = { setTotalBill, setTotalSplit, setTotalTip, setSplitTip };
 
